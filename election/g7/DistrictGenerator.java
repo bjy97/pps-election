cdca43f1f657de961c9452f998b7463fa735bd59
zhanzhicheng <554813245@qq.com>
2019-10-10 18:56:55 -0400

    Update method interface.

--- election/g7/DistrictGenerator.java
+++ election/g7/DistrictGenerator.java
@@ -9,10 +9,11 @@ public class DistrictGenerator implements election.sim.DistrictGenerator {
     private Random random;
     private int numVoters, numParties, numDistricts;
     private double eps = 1E-7;
-    private List<Map<Polygon2D, List<Voter>>> polyganList;
-    private Map<Integer, Polygon2D> polygonMap;
-    private Map<Integer, List<Voter>> voterMap;
-    private Map<Integer, Boolean> checkMap;
+    private List<Map<Polygon2D, List<Voter>>> polyganList = new ArrayList<>();
+    private Map<Integer, Polygon2D> polygonMap = new HashMap<>();
+    private Map<Integer, List<Voter>> voterMap = new HashMap<>();
+    private Map<Integer, Boolean> checkMap = new HashMap<>();
+    private int partyToWin = 1;
 
     public List<Voter> sortByXCoordinate(List<Voter>voters){
         Collections.sort(voters, new Comparator<Voter>() {
@@ -77,63 +78,54 @@ public class DistrictGenerator implements election.sim.DistrictGenerator {
                 result.add(polygon);
             }
         }
-            int i = 0;
-            for (Map<Polygon2D, List<Voter>> map : polyganList) {
-                for (Map.Entry<Polygon2D, List<Voter>> entry: map.entrySet()){
-                    polygonMap.put(i, entry.getKey());
-                    voterMap.put(i++, entry.getValue());
+        int i = 0;
+        for (Map<Polygon2D, List<Voter>> map : polyganList) {
+            for (Map.Entry<Polygon2D, List<Voter>> entry: map.entrySet()){
+                polygonMap.put(i, entry.getKey());
+                voterMap.put(i++, entry.getValue());
+                checkMap.put(i, false);
+            }
+        }
+
+        for (Map.Entry<Integer, Polygon2D> entry : polygonMap.entrySet()) {
+            int id = entry.getKey();
+            if (!checkMap.get(id) && !isSwingState(id)) {
+                checkMap.put(id, true);
+                Map<Integer, double[]> adjacentDistricts = getAdjacentDistricts(id);
+                Polygon2D swing = entry.getValue();
+                for (Map.Entry<Integer, double[]> adjacentDistrict : adjacentDistricts.entrySet()) {
+                    int otherId = adjacentDistrict.getKey();
+                    double[] edge = adjacentDistrict.getValue();
+                    double x1 = edge[0], y1 = edge[1], x2 = edge[2], y2 = edge[3];
+                    double len = Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+                    double width = getWidth(len);
+
                 }
             }
-
-            System.out.println(result.size());
-            return result;
         }
+
+        System.out.println(result.size());
+        return result;
+    }
 
         private double getWidth(double len) {
             return 0.0;
         }
 
-        private List<Map<Integer, Double>> getAdjacentDistricts(int id) {
-            List<Map<Integer, Double>> list = new ArrayList<>();
+        //Return the index of the matrix with the a double array representting the vertex for the edge.
+        // [0] x1, [1] y1, [2] x2, [3] y2
+        private Map<Integer, double[]> getAdjacentDistricts(int id) {
+            Map<Integer, double[]> list = new HashMap<>();
             return list;
         }
 
-        private boolean isSwingState(Polygon2D polygon2D, List<Voter> voters, int partyToWin) {
+        // partyToWin is global variable now
+        private boolean isSwingState(int id) {
             return false;
         }
 
         //Check population is valid for two polygon2 and if how beneficial it is for digging.
-        private boolean isValidGerrymander(Polygon2D swing, Polygon2D other, List<Voter> voters, int partyToWin) {
+        private boolean isValidGerrymander(int swingId, int otherId, Polygon2D swing, Polygon2D other) {
             return false;
         }
 }
-
-        /*            //sort voters by x-coordinate
-            List<Voter> voter_by_x = sortByXCoordinate(voter_by_y);
-            List<Double> x_coordinates = new ArrayList<>();
-            //draw vertical lines in each stripe area
-            double top = btm;
-            btm = Math.max(voter_by_x.get(voter_by_x.size() - 1).getLocation().getY() - eps, 0);
-            double preX = btm / Math.sqrt(3);
-            for (int j = 1; j <= blockEachStripe; j++) {
-                if (j == 1) {
-                    double x = voter_by_x.get(peopleInBlock*(j)).getLocation().getX() + eps;
-                    Polygon2D polygon = new Polygon2D();
-                    polygon.append(preX, btm);
-                    polygon.append(x, btm);
-                    polygon.append(x, (x - preX)*Math.sqrt(3) + btm);
-                    result.add(polygon);
-                    if (j == blockEachStripe) {
-
-                    }
-                    preX += x;
-                    preY +=
-                }
-                if (j == blockEachStripe) {
-                    double x = voter_by_x.get(peopleInBlock*(j)).getLocation().getX() + eps;
-                    Polygon2D polygon = new Polygon2D();
-                    polygon.append(preX, btm);
-                    polygon.append(x, btm);
-                    polygon.append(x, (x - preX)*Math.sqrt(3) + btm);
-                    result.add(polygon);
-                }*/
